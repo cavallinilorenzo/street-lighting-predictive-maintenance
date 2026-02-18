6b02e6ca25d116786ed6ec949516527b04adf4bf
Matteo Benassi <71072396+itsmrma@users.noreply.github.com>
2026-02-18 19:41:59 +0100

    html proof of concept

--- core/views.py
+++ core/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import LampioneManutenzione
 
 # Create your views here.
+def home(request):
+    # QUERY: SELECT tcs_descr, COUNT(*) as totale 
+    #        FROM core_lampionemanutenzione 
+    #        GROUP BY tcs_descr 
+    #        ORDER BY totale DESC LIMIT 5
+    
+    top_5_motivi = (
+        LampioneManutenzione.objects
+        .values('tcs_descr')                  # Raggruppa per descrizione guasto
+        .annotate(totale=Count('id'))         # Conta quanti ID ci sono per gruppo
+        .order_by('-totale')                  # Ordina decrescente (dal più alto)
+        .exclude(tcs_descr__isnull=True)      # Escludi i nulli (pulizia dati)
+        [:5]                                  # Prendi solo i primi 5 (LIMIT 5)
+    )
+
+    context = {
+        'top_5_motivi': top_5_motivi
+    }
+    return render(request, 'core/home.html', context)
